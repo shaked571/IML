a1efba2d1ff132cbfff6a810d34a1adea48fa44c
shaked571 <shakedgreenfeld@gmail.com>
2018-04-25 19:12:56 +0300

    finish mission 1 - need to reorder photo

--- ex1/ex2.py
+++ ex1/ex2.py
@@ -36,26 +36,33 @@ def main():
     train_error = []
     test_error = []
     for x in range(1, 100):
-        training_data = processed_data.sample(frac=(x/100))
-        test_data = processed_data.drop(training_data.index)
+        rows = np.random.rand(len(processed_data)) < x / 100
+        training_data = processed_data[rows]
+        test_data = processed_data[~rows]
 
-        data_matrix, price_vector = get_data_matrix_and_price_vec(training_data)
-        testing_data_matrix, testing_price_vector = get_data_matrix_and_price_vec(test_data)
+        price_vector = training_data[[PRICE]]
+        data_matrix = training_data.drop([PRICE], axis=1)
 
-        prediction_w = np.dot(data_matrix, price_vector)
+        testing_price_vector = test_data[[PRICE]]
+        testing_data_matrix = test_data.drop([PRICE], axis=1)
 
-        testing_preindiction_value_vector = np.dot(testing_data_matrix, prediction_w)
+        prediction_w = np.dot(np.linalg.pinv(data_matrix), price_vector)
 
-        train_mse = get_mse(prediction_value_vector, price_vector)
-        testing_mse = get_mse(testing_prediction_value_vector, testing_price_vector)
+        train_vec = np.dot(data_matrix, prediction_w)
+        test_vec = np.dot(testing_data_matrix, prediction_w)
+
+        testing_mse = get_mse(test_vec, testing_price_vector)
         test_error.append(testing_mse)
+
+        train_mse = get_mse(train_vec, price_vector)
         train_error.append(train_mse)
 
     show_graph(train_error, test_error)
 
 
-def show_graph(input_array1, test_error):
-    plt.plot(input_array1, 'g', test_error, 'r')
+def show_graph(train_error, test_error):
+    plt.plot(train_error, 'g', test_error, 'r')
+    plt.title("train error(g) vs test error(r) ")
     plt.show()
 
 def get_mse(y, y_roof):
@@ -63,21 +70,18 @@ def get_mse(y, y_roof):
 
 
 def get_predict_vec(matrix, vector):
-    return np.dot(np.transpose(matrix), vector)
+    return np.dot(matrix, np.transpose(vector))
 
 
-def get_data_matrix_and_price_vec(training_data):
-    panda_price_vector = training_data[[PRICE]]
-    data = training_data.drop([PRICE], axis=1)
-    return np.linalg.pinv(data), panda_price_vector.values
+
 
 
 def process_data(row_data):
     temp_processed_data = clean_unwanted_columns(row_data)
-    temp_processed_data = temp_processed_data[temp_processed_data[PRICE] > 0]
     remove_data(temp_processed_data)
     temp_processed_data = temp_processed_data[temp_processed_data[SQFT_LIVING] > 250]
     temp_processed_data = pd.get_dummies(temp_processed_data, columns=[ZIPCODE])
+    temp_processed_data.insert(0, 'w_0', 1)
     processed_data = temp_processed_data.dropna(axis=0, how="any")
     return processed_data
 
@@ -91,11 +95,10 @@ def remove_data(origin_matrix):
                                      (origin_matrix.sqft_above <= 0) |
                                      (origin_matrix.sqft_basement <= 0)].index,
                        inplace=True)
-    origin_matrix.dropna(inplace=True)
 
 def clean_unwanted_columns(row_data):
     return row_data.drop(
-        [ID, DATE, WATERFRONT, VIEW, CONDITION, YEAR_BUILT, FLOORS, YEAR_RENOVATED, LAT, LONG], axis=1)
+        [ID, DATE, WATERFRONT, VIEW, LAT, LONG], axis=1)
 
 
 def get_row_data():
