1af39da64947910dd84597776184c113d9ed8e43
shaked571 <shakedgreenfeld@gmail.com>
2018-06-09 17:52:15 +0300

    Q3 finish and work

--- ex4/adaboost.py
+++ ex4/adaboost.py
@@ -2,12 +2,6 @@
 ===================================================
      Introduction to Machine Learning (67577)
 ===================================================
-
-Skeleton for the AdaBoost classifier.
-
-Author: Noga Zaslavsky
-Edited: Yoav Wald, May 2018
-
 """
 import numpy as np
 
@@ -29,7 +23,21 @@ class AdaBoost(object):
         """
         Train this classifier over the sample (X,y)
         """
+        sample_num = X.shape[0]
+        distribution = np.full(sample_num, 1/sample_num)
+        for i in range(self.T):
+            self.h[i] = self.WL(distribution, X, y)
+            error_t = np.dot(distribution, (y != self.h[i].predict(X)))
+            self.w[i] = self.get_weight(error_t)
+            y_predicted = self.h[i].predict(X)
+            distribution = self.get_distribution(distribution, i, y, y_predicted)
 
+    def get_weight(self, error_t):
+        return 0.5 * np.log((1 / error_t) - 1)
+
+    def get_distribution(self, distribution, i, y, y_predicted):
+        return (distribution * np.exp(-self.w[i] * y * y_predicted)) / (
+        distribution * np.exp(-self.w[i] * y * y_predicted)).sum()
 
     def predict(self, X):
         """
@@ -37,6 +45,7 @@ class AdaBoost(object):
         -------
         y_hat : a prediction vector for X
         """
+        return np.sign(np.array([np.dot(self.w[t], self.h[t].predict(X)) for t in range(self.T)]).sum(axis=0))
 
     def error(self, X, y):
         """
@@ -44,3 +53,6 @@ class AdaBoost(object):
         -------
         the error of this classifier over the sample (X,y)
         """
+        y_hat = self.predict(X)
+        y = np.array(y)
+        return sum(y[i] != y_hat[i] for i in range(len(y))) / len(y)
--- ex4/ex4_runme.py
+++ ex4/ex4_runme.py
@@ -11,9 +11,49 @@ Date: May, 2018
 """
 
 import numpy as np
+from SynData import *
+from ex4_tools import decision_boundaries, h_opt, DecisionStump
+from matplotlib.pyplot import *
+import adaboost as aba
+
 
 def Q3(): # AdaBoost
-    # TODO - implement this function
+    X_train = np.loadtxt("SynData/X_train.txt")
+    X_test = np.loadtxt("SynData/X_test.txt")
+    X_val = np.loadtxt("SynData/X_val.txt")
+    y_train = np.loadtxt("SynData/y_train.txt")
+    y_test = np.loadtxt("SynData/y_test.txt")
+    y_val = np.loadtxt("SynData/y_val.txt")
+    val_error = []
+    train_error =[]
+    for T in range(1, 205, 5):
+        adaboost = aba.AdaBoost(DecisionStump, T)
+        adaboost.train(X_train, y_train)
+        train_error.append(adaboost.error(X_train, y_train))
+        val_error.append(adaboost.error(X_val, y_val))
+
+    plot(list(range(1, 205, 5)), train_error)
+    plot(list(range(1, 205, 5)), val_error)
+    xlabel("Iteration_num")
+    ylabel("error")
+    legend(["Training Error", "Validation Error"], loc=7)
+    show()
+
+    figure(1)
+    ion()
+    for index, T in enumerate([1, 5, 10, 50, 100, 200]):
+        adaboost = aba.AdaBoost(DecisionStump, T)
+        adaboost.train(X_train, y_train)
+        subplot(2, 3, index + 1)
+        decision_boundaries(adaboost, X_train, y_train, "T = " + str(T))
+
+    pause(10)
+
+    best_T = val_error.index(np.min(val_error)) * 5
+    print(best_T)
+    ab = aba.AdaBoost(DecisionStump, best_T)
+    ab.train(X_train, y_train)
+    print(ab.error(X_test, y_test))
     return
 
 def Q4(): # decision trees
@@ -24,6 +64,12 @@ def Q5(): # spam data
     # TODO - implement this function
     return
 
+def main():
+    Q3()
+    Q4()
+    Q5()
+
 if __name__ == '__main__':
-    # TODO - run your code for questions 3-5
-    pass
+   main()
+
+
